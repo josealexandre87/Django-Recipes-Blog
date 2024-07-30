116f88b49b44add0fbea0e14dc578a9ddd627dce
josealexandre87 <jose.josealexandre87@gmail.com>
2024-07-30 05:56:14 -0300

    Removidos contato e sobre de views.py e urls.py, pois eram exemplos

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,10 +3,8 @@
 #E todas essas URLs estão sendo importadas a partir daqui lá para a pasta de entrada projeto/urls.py usando `path('', include('recipes.urls'))`, da lista de urlpatterns[].
 
 from django.urls import path
-from recipes.views import home, contato, sobre # importa do app recipes funções que renderizam as páginas
+from recipes.views import home # importa do app recipes funções que renderizam as páginas
 
 urlpatterns = [
     path('', home), # importa de recipes/views a função home e o que retorna dela.
-    path('contato/', contato), # importa de recipes/views a função contatoe o que retorna dela
-    path('sobre/', sobre), # importa de recipes/views a função sobre e o que retorna dela.
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse # para receber o que está nas views como resposta do servidor.
 
 # Create your views here.
 def home(request): #usar namespaces (pastas) pare evitar conflitos de onde vai vir o arquivo html.
     return render(request, 'recipes/home.html') # tem que criar a pasta 'templates' no app recipes para o Django entender que tem um arquivo .html para renderizar e, também, tem que ser informado em projeto/settings.py a existência do app 'recipes'.
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-def sobre(request):
-    return HttpResponse('SOBRE')
